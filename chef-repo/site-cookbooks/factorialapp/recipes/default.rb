1a4750a069f9ec30c224f170f3309a3da2ffbd28
root <root@devops-project>
2015-12-15 22:16:13 +0000

    First draft

---
+++ chef-repo/site-cookbooks/factorialapp/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: factorialapp
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+app_path = "/opt/airpair-devops-toolstack"
+if not ::File.exists?(app_path)
+  app_path = "/vagrant"
+end
+
+package "python-pip"
+
+execute "install-python-module" do
+  command "pip install -r #{app_path}/requirements.txt"
+end
